bdade6938a4756d3957a5a980cd9cfe86e000c5e
Samuele Mazzei <samuele.mazzei@studenti.unitn.it>
2021-02-03 16:11:23 +0000

    Forth commit

--- main.cpp
+++ main.cpp
@@ -38,18 +38,45 @@ typedef struct Tnodo{
   }
 }Tnodo;
 
+typedef struct TipoCoda {
+  Tnodo* head;
+  Tnodo* tail;
+
+  TipoCoda(){
+    head=NULL; 
+    tail=NULL;
+  }
+} TipoCoda;
+
+
 
 Tnodo* insertFirst(Tnodo* s, Tdato d);
 Tnodo* removeFirst(Tnodo* s);
+Tnodo* insertLast(Tnodo* s, Tdato d);
+Tnodo* removeLast(Tnodo* s);
+Tnodo* insertOrder(Tnodo* s, Tdato d);
+Tnodo* removeSearch(Tnodo* s, Tdato d);
 void stampa(Tnodo* s);
 
 
 int main() {
-  Tnodo* listaLIFO = NULL;
-  listaLIFO = insertFirst(listaLIFO, Tdato());
-  stampa(listaLIFO);
-  listaLIFO = removeFirst(listaLIFO);
-  stampa(listaLIFO);
+  Tnodo* lista = NULL;
+  lista = insertFirst(lista, Tdato(1));
+  lista = insertFirst(lista, Tdato(2));
+  lista = insertFirst(lista, Tdato(3));
+  stampa(lista);
+  lista = removeFirst(lista);
+  stampa(lista);
+  lista = insertLast(lista, Tdato(4));
+  stampa(lista);
+  lista = removeLast(lista);
+  stampa(lista);
+  lista = insertFirst(lista, Tdato(8));
+  stampa(lista);
+  lista = insertOrder(lista, Tdato(6));
+  stampa(lista);
+  lista = removeSearch(lista, Tdato(6));
+  stampa(lista);
 }
 
 Tnodo* insertFirst(Tnodo* s, Tdato d){
@@ -73,7 +100,93 @@ void stampa(Tnodo* s){
   Tnodo* p = s;
   while(p != NULL){
     p->stampa();
+    cout << endl;
     p = p->next;
   }
   cout << endl;
+}
+
+Tnodo* insertLast(Tnodo* s, Tdato d){
+  Tnodo* q = new Tnodo();
+  q->dato = d;
+  if(s == NULL){
+    return q;
+  }
+  Tnodo* p = s;
+  while(p->next != NULL){
+    p = p->next;
+  }
+  p->next = q;
+  return s;
+}
+
+Tnodo* removeLast(Tnodo* s){
+  if(s == NULL){
+    return NULL;
+  }
+  if(s->next == NULL){
+    delete s;
+    return NULL;
+  }
+  Tnodo* p = s;
+  while(p->next->next != NULL){
+    p = p->next;
+  }
+  delete p->next;
+  p->next = NULL;
+  return s;
+}
+
+Tnodo* insertOrder(Tnodo* s, Tdato d){
+  if(s == NULL){
+    Tnodo* q = new Tnodo();
+    return q;
+  }
+  if(s->dato.numero < d.numero){ //condizione
+    return insertFirst(s, d);
+  }
+  Tnodo* p = s;
+  while(p->next != NULL && p->next->dato.numero > d.numero){
+    p = p->next;
+  }
+  Tnodo* q = new Tnodo(d, p->next);
+  p->next = q;
+  return s;
+}
+
+Tnodo* removeSearch(Tnodo* s, Tdato d){
+  if (s != NULL) {
+    Tnodo* q = s;
+    if (q->dato.numero == d.numero) { //condizione
+      s = s->next;
+    delete q; 
+    return s;
+    } else {
+      while(q->next != NULL) {
+        if (q->next->dato.numero == d.numero) {
+          Tnodo* r = q->next;
+          q->next = q->next->next;
+          delete r;
+          return s;
+        }
+        if (q->next != NULL){ 
+          q=q->next; 
+        }
+      }
+    }
+  }
+  return NULL; 
+}
+
+void put(TipoCoda* p, Tdato d) {
+  TipoCoda* n;
+  n = p;
+  Tnodo* q =new Tnodo(d,NULL);
+  if (n->tail==NULL){ //lista vuota
+    n->head = q; 
+  } else { 
+      n->tail->next = q; 
+    }
+  n.tail = q;
+  p = n;
 }
